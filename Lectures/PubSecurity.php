84b7a08d4d9e44811c2cbe2e8f9ec17979439b45
Daniel Toll <daniel@familjentoll.se>
2013-09-02 14:52:33 +0200

    Lecture 1
    Example of PHPDocumentor comments on return values and parameters with
    type
    
    if statement and comparison operator

---
+++ Lectures/PubSecurity.php
@@ -0,0 +1,16 @@
+<?php
+
+class PubSecurity {
+	
+	/**
+	* @param integer $age age in years
+	* @return boolean
+	*/
+	public function isOk($age) {
+		if ($age >= 18) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
